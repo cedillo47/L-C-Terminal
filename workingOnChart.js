dbdb7e1397cff270952536dc4e49bc8e0f88ba39
leo <cedillo47@gmail.com>
2022-03-02 01:22:22 -0500

    figuring out how to work with chart

--- workingOnChart.js
+++ workingOnChart.js
@@ -15,6 +15,8 @@ window.addEventListener('DOMContentLoaded', () => {
     const baseURLForAlphvantage = 'https://www.alphavantage.co/'
     
 
+    const ctx = document.getElementById('myChart').getContext('2d');
+
     const graphData = async () => {
         let dateData = [];
         let priceData =[];
@@ -31,6 +33,7 @@ window.addEventListener('DOMContentLoaded', () => {
             // console.log(row)
             // const price = row[]
             console.log(date,price)
+            
         })
 
         console.log("hello")
@@ -55,11 +58,44 @@ window.addEventListener('DOMContentLoaded', () => {
         // }
         // console.log("this is the date:", dateData);
         // console.log("this is the price:", priceData);
+    //     const myChart = new Chart(ctx, {
+    //     type: 'bar',
+    //     data: {
+    //         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    //         datasets: [{
+    //             label: '# of Votes',
+    //             data: [12, 19, 3, 5, 2, 3],
+    //             backgroundColor: [
+    //                 'rgba(255, 99, 132, 0.2)',
+    //                 'rgba(54, 162, 235, 0.2)',
+    //                 'rgba(255, 206, 86, 0.2)',
+    //                 'rgba(75, 192, 192, 0.2)',
+    //                 'rgba(153, 102, 255, 0.2)',
+    //                 'rgba(255, 159, 64, 0.2)'
+    //             ],
+    //             borderColor: [
+    //                 'rgba(255, 99, 132, 1)',
+    //                 'rgba(54, 162, 235, 1)',
+    //                 'rgba(255, 206, 86, 1)',
+    //                 'rgba(75, 192, 192, 1)',
+    //                 'rgba(153, 102, 255, 1)',
+    //                 'rgba(255, 159, 64, 1)'
+    //             ],
+    //             borderWidth: 1
+    //         }]
+    //     },
+    //     options: {
+    //         scales: {
+    //             y: {
+    //                 beginAtZero: true
+    //             }
+    //         }
+    //     }
+    // });
 
-
-    }
+}
     // timestamp: '2022-03-01', open: '164.6950'
-    // graphData()
+    graphData()
 
 
 
